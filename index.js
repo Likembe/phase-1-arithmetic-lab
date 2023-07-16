47e0b6b6da7073c6566c98f7e61119b946f389ce
Alpha <alpha.likembe@student.moringaschool.com>
2023-07-16 07:11:42 +0300

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+// // // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
+console.log(multiply);
+
+
+const random = Math.floor(Math.random()*100)+1;
+console.log(random);
+
+const num3 = 14;
+const num4 = 5;
+const mod = (num3 % num4);
+
+console.log(mod);
+
+const set = [2, 6, 16, 8, 18, 20]
+const max = Math.max(...set)
+
+console.log(max);
+
+
